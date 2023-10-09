8bfd3dae60be62507ced4f095bbe0a2a696284bc
Firepal <firepal@cyberdude.com>
2023-10-09 17:05:42 +0200

    Remove RayCast3D node requirement, cleanup

--- player_source_movement.gd
+++ player_source_movement.gd
@@ -4,104 +4,113 @@ extends CharacterBody3D
 #NOTE 2: If you want to recreate even more source-like movement use a box collision shape
 
 @export_category("Movement")
-#Constants to make editing game feel easier
-@export var MAX_G_SPEED : float = 7.0
-@export var MAX_G_ACCELERATION : float = 20.0 * MAX_G_SPEED
-@export var MAX_A_SPEED : float = 0.5
-@export var MAX_A_ACCELERATION : float = 100.0
-@export var JUMP_FORCE : float = 4.5
-@export var GRAVITY_FORCE : float = 15.0
-@export var MAX_SLOPE : float = deg_to_rad(46.0)
+@export var MAX_G_SPEED := 10.0
+@export var MAX_G_ACCEL := 20.0 * MAX_G_SPEED
+@export var MAX_AIR_SPEED := 0.5
+@export var MAX_AIR_ACCEL := 100.0
+@export var JUMP_FORCE := 4.5
+@export var GRAVITY_FORCE := 15.0
+@export var MAX_SLOPE := deg_to_rad(46.0)
 
-#Store horizontal and aerial velocity differently
-var hvelocity : Vector2 = Vector2.ZERO
-var vvelocity : float = 0.0
+@export_category("Settings")
+## The local position of the raycast used to check for the floor.
+@export var FLOOR_RAY_POS := Vector3.ZERO
+## How far down the floor raycast will reach out for collisions.
+@export var FLOOR_RAY_REACH := 0.2
 
 @export_category("Nodes")
-#Raycast for the ground and a custom "is_on_floor variable"
-@export var FLOOR_CAST : RayCast3D
-var on_floor : bool = false
+@export var cam_pivot : Node3D
 
-#Get the camera pivot (needed to not rotate the character)
-@export var CAMERA_PIVOT : Node3D
+var on_floor := false
+var floor_check := {}
 
-#Physics process, where the magic source magic happens
+func _check_floor() -> bool:
+	var origin = global_position + FLOOR_RAY_POS
+	var target = Vector3.DOWN * FLOOR_RAY_REACH
+	
+	var query = PhysicsRayQueryParameters3D.create(origin, origin + target)
+	
+	var check = get_world_3d().direct_space_state.intersect_ray(query)
+	
+	var collided = check.size() > 0
+	if collided: floor_check = check
+	return collided
+
+func get_slope_angle(normal): return normal.angle_to(up_direction)
+
+# Physics process, where the source magic happens
 func _physics_process(delta):
-	#Detect if is on floor
-	#In case of not being on it (angle to ground or just not touching it) set to false
+	# Decompose vector
+	var vel_planar := Vector2(velocity.x,velocity.z)
+	var vel_vertical := velocity.y
+	
+	# Detect if is on floor
+	# In case of not being on it (angle to ground or just not touching it) set to false
 	if on_floor:
-		if FLOOR_CAST.is_colliding():
-			if Vector3.UP.angle_to(FLOOR_CAST.get_collision_normal()) < MAX_SLOPE:
-				on_floor = true
-			else:
-				on_floor = false
-		else:
-			on_floor = false
+		var collided = _check_floor()
+		var slope_angle = get_slope_angle(floor_check.normal) 
+		on_floor = collided and slope_angle < MAX_SLOPE
 	
-	#Get input vector of the player and rotate it depending on the camera's rotation
-	var wish_dir : Vector2 = Input.get_vector("player_l","player_r","player_f","player_b")
-	wish_dir = wish_dir.rotated(-CAMERA_PIVOT.rotation.y)
+	# Get input vector of the player
+	# and rotate it depending on the camera's rotation
+	var wish_dir := Input.get_vector("player_l","player_r","player_f","player_b")
+	wish_dir = wish_dir.rotated(-cam_pivot.rotation.y)
 	
-	#Jump
-	if on_floor and Input.is_action_pressed("player_jump"):
+	
+	if on_floor and Input.is_action_pressed("player_jump"): # Jump
 		on_floor = false
-		vvelocity = JUMP_FORCE
+		vel_vertical = JUMP_FORCE
 	
-	#Basic gravity
-	if !on_floor:
-		vvelocity -= GRAVITY_FORCE * delta
-	#Basic friction
-	else:
-		hvelocity -= hvelocity.normalized() * delta * (MAX_G_ACCELERATION / 2.0)
-		#Fully stop if there is no input
-		if hvelocity.length() < 1.0 and wish_dir.length_squared() < 0.01:
-			hvelocity = Vector2.ZERO
+	# Apply drag/friction
+	if not on_floor: vel_vertical -= GRAVITY_FORCE * delta
+	else: 
+		vel_planar -= vel_planar.normalized() * delta * (MAX_G_ACCEL / 2.0)
+		# Fully stop if there is no velocity
+		if vel_planar.length_squared() < 1.0 and wish_dir.length_squared() < 0.01:
+			vel_planar = Vector2.ZERO
 	
-	#Where the source magic happens.
-	#This dot is the cause for all the movement tricks in source games
-	#Good reference for this: https://www.youtube.com/watch?v=v3zT3Z5apaM
-	var current_speed : float = hvelocity.dot(wish_dir)
-	#Select the max speed and acceleration depending if its on floor or not
-	var MAX_SPEED : float = MAX_G_SPEED if on_floor else MAX_A_SPEED
-	var MAX_ACCELERATION : float = MAX_G_ACCELERATION if on_floor else MAX_A_ACCELERATION
-	#The magic source function
-	var add_speed : float = clamp(MAX_SPEED - current_speed, 0.0, MAX_ACCELERATION * delta)
+	# Where the source magic happens.
+	# This dot is the cause for all the movement tricks in source games
+	# Good reference for this: https://www.youtube.com/watch?v=v3zT3Z5apaM
+	var current_speed = vel_planar.dot(wish_dir)
 	
-	#Add the speed to the horizontal speed
-	hvelocity += wish_dir * add_speed
+	# Select the max speed and acceleration depending if its on floor or not
+	var max_speed = MAX_G_SPEED if on_floor else MAX_AIR_SPEED
+	var max_accel = MAX_G_ACCEL if on_floor else MAX_AIR_ACCEL
 	
-	#Set the velocity to our horizontal and vertical speeds
-	velocity.x = hvelocity.x; velocity.z = hvelocity.y
-	velocity.y = vvelocity
+	# The magic source function
+	var add_speed = clamp(max_speed - current_speed, 0.0, max_accel * delta)
 	
-	#Move and collide is better to handle every aspect of how the velocity is modified
-	var collision : KinematicCollision3D = move_and_collide(velocity*delta)
+	vel_planar += wish_dir * add_speed
+	
+	# We're done working with our decomposed velocity
+	# Set the velocity so we can finally simulate our physics
+	velocity = Vector3(vel_planar.x,vel_vertical,vel_planar.y)
+	
+	# We do our own sliding, so we use move_and_collide
+	var collision = move_and_collide(velocity * delta)
 	if collision:
-		#Slide the remaining movement and move
+		# Slide the remaining movement and move
 		move_and_collide(collision.get_remainder().slide(collision.get_normal()))
-		#Slide the speed against the collision if 
-		if !on_floor:
+		
+		if not on_floor:
 			velocity = velocity.slide(collision.get_normal())
-			#Detect if it has collided with the ground
-			if collision.get_normal().angle_to(Vector3.UP) < MAX_SLOPE:
+			
+			var slope_angle = get_slope_angle(collision.get_normal())
+			if slope_angle < MAX_SLOPE:
 				on_floor = true
 				velocity.y = 0.0
 	else:
-		#Snap to ground
 		if on_floor:
-			#Detect that in fact, the player is on the ground
-			FLOOR_CAST.force_raycast_update()
-			if FLOOR_CAST.is_colliding():
-				move_and_collide(FLOOR_CAST.get_collision_point()-global_position)
-	
-	#Set the modified velocity onto the separate variables again
-	hvelocity.x = velocity.x; hvelocity.y = velocity.z
-	vvelocity = velocity.y
+			# Detect that in fact, the player is on the ground
+			# and snap to ground accordingly
+			if _check_floor():
+				move_and_collide(floor_check.position - global_position)
 
-#Custom input function (swap for what you need)
+# Custom input function (swap for what you need)
 func _input(event):
-	#Rotate the camera according to mouse input
+	# Rotate the camera according to mouse input
 	if event is InputEventMouseMotion:
-		CAMERA_PIVOT.rotation.y += -event.relative.x * 0.002
-		CAMERA_PIVOT.rotation.x += -event.relative.y * 0.002
-		CAMERA_PIVOT.rotation_degrees.x = clamp(CAMERA_PIVOT.global_rotation_degrees.x, -89.0 , 89.0)
+		cam_pivot.rotation.y += -event.relative.x*0.001
+		cam_pivot.rotation.x += -event.relative.y*0.001
+		cam_pivot.rotation_degrees.x = clamp(cam_pivot.rotation_degrees.x, -89.0 , 89.0)
